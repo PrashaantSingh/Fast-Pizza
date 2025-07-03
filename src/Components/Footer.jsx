8712208e19df7a3da1d69a5500f384658ad152c5
Prashant singh <110338021+PrashaantSingh@users.noreply.github.com>
2025-07-03 20:34:30 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="h-10 bg-slate-900 text-gray-50 text-center flex justify-center items-center text-sm sticky">
       {" "}
-      &copy;2024 Fast Pizza. All Rights Reserved.
+      &copy;2024 SliceCart. All Rights Reserved.
     </div>
   );
 }
